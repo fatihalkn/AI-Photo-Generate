547c347165e7e6f90a081f728b7b69fc8dcf0807
Fatih <151558909+fatihalkn@users.noreply.github.com>
2024-03-19 20:27:52 +0300

    Complate HomePage

--- AI-PhotoGenerate/App/SceneDelegate.swift
+++ AI-PhotoGenerate/App/SceneDelegate.swift
@@ -17,8 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         self.window?.makeKeyAndVisible()
-        let vc = HomePageController()
+        let vc = TabBarController()
         let nav = UINavigationController(rootViewController: vc)
+        window.rootViewController = nav
         self.window?.rootViewController = nav
     }
 
---
+++ AI-PhotoGenerate/CustomCells/SelectStyleCell.swift
@@ -0,0 +1,68 @@
+//
+//  SelectStyleCell.swift
+//  AI-PhotoGenerate
+//
+//  Created by Fatih on 19.03.2024.
+//
+
+import UIKit
+
+class SelectStyleCell: UICollectionViewCell {
+    static let identifier = "SelectStyleCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        configureCons()
+    }
+        
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+     let styleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius  = 16
+        return imageView
+    }()
+    
+     let styleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 15, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupUI() {
+        self.addSubview(styleImageView)
+        self.addSubview(styleLabel)
+    }
+}
+
+//MARK: - Configure Constrains
+extension SelectStyleCell {
+    
+    func configureCons() {
+        NSLayoutConstraint.activate([
+            styleImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            styleImageView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            styleImageView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            styleImageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            styleLabel.centerXAnchor.constraint(equalTo: styleImageView.centerXAnchor),
+            styleLabel.topAnchor.constraint(equalTo: styleImageView.bottomAnchor),
+            styleLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+            
+        ])
+    }
+}
